fab6899af252131b73f81c48af2b6ab3f798b699
Ninad2811 <ninadchavan2811@gmail.com>
2025-11-30 19:26:24 +0530

    using the compo in the app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import Hello from './compo/Hello';
 import Welcome from './compo/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Welcome/>
-
+      <Hello/>
     </div>
   );
 }
