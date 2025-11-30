97ff00c725042dab83d162149303eb6e1ced450b
Ninad2811 <156406519+Ninad2811@users.noreply.github.com>
2025-11-30 18:26:49 +0530

    learning git

---
+++ src/compo/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Welcom() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero sequi,
+        culpa iste placeat cum numquam quam perspiciatis dolore dicta
+        praesentium necessitatibus voluptas labore veritatis eum! Minus quaerat
+        necessitatibus voluptatibus culpa?
+      </h1>
+    </div>
+  );
+}
+
+export default Welcom;
