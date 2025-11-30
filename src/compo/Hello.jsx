b9d82294031ed81a024bce728373705fcf06f142
Ninad2811 <ninadchavan2811@gmail.com>
2025-11-30 19:16:12 +0530

    updated h1

---
+++ src/compo/Hello.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Hello() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam quos,
+        odit obcaecati temporibus id quis, soluta labore et earum similique
+        deleniti unde libero voluptatibus vero ad. Molestiae deleniti sint
+        tempore et saepe fuga officiis vitae delectus, cumque nemo
+        necessitatibus suscipit eveniet hic eligendi magni odit aperiam
+        voluptatem. Exercitationem, similique atque.
+      </h1>
+    </div>
+  );
+}
+
+export default Hello;
